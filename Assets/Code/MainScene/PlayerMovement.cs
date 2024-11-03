8b168b5166db9a854e67668edec454dbd4f4855b
lhany2007 <lhany121407@gmail.com>
2024-11-03 23:17:57 +0900

    플레이어 대각선 애니매이션 제거 및 플레이어 z축 고정

--- Assets/Code/MainScene/PlayerMovement.cs
+++ Assets/Code/MainScene/PlayerMovement.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed;
+
     Vector2 inputVector;
     Rigidbody2D rigid;
     Animator animator;
 
+    // 애니메이션 파라미터 이름
     const string IS_MOVING = "IsMoving";
     const string MOVE_Y = "moveY";
     const string IS_MOVING_X = "isMovingX";
     const string IS_MOVING_Y = "isMovingY";
-    const string IS_UNDER_DIAGONAI_UP = "isUnderDiagonalUp";
-    const string IS_UNDER_DIAGONAI_DOWN = "isUnderDiagonalDown";
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -27,58 +29,37 @@ public class PlayerMovement : MonoBehaviour
     {
         inputVector.x = Input.GetAxisRaw("Horizontal");
         inputVector.y = Input.GetAxisRaw("Vertical");
-        // 대각선 움직임 정규화
+
+        // 대각선 이동 시 정규화
         if (inputVector.magnitude > 1)
         {
             inputVector.Normalize();
         }
-        // 좌우반전
+
+        // 좌우 반전
         if (inputVector.x != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(inputVector.x), 1, 1);
         }
+
+        UpdateAnimationStates();
     }
+
     void UpdateAnimationStates()
     {
+        // 캐릭터가 이동 중인지 확인
         if (inputVector.magnitude > 0)
         {
             animator.SetBool(IS_MOVING, true);
-
-            // 대각선 움직임 확인 (x와 y가 모두 0이 아닌 경우)
-            if (Mathf.Abs(inputVector.x) > 0.1f && Mathf.Abs(inputVector.y) > 0.1f)
-            {
-                if (inputVector.y > 0)
-                {
-                    animator.SetBool(IS_UNDER_DIAGONAI_UP, true);
-                    animator.SetBool(IS_UNDER_DIAGONAI_DOWN, false);
-                }
-                else
-                {
-                    animator.SetBool(IS_UNDER_DIAGONAI_DOWN, true);
-                    animator.SetBool(IS_UNDER_DIAGONAI_UP, false);
-                }
-            }
-            else
-            {
-                // 대각선이 아닌 경우 두 상태 모두 false
-                animator.SetBool(IS_UNDER_DIAGONAI_UP, false);
-                animator.SetBool(IS_UNDER_DIAGONAI_DOWN, false);
-            }
-
             animator.SetFloat(MOVE_Y, inputVector.y);
         }
         else
         {
-            // 움직임이 없는 경우 모든 상태 리셋
             animator.SetBool(IS_MOVING, false);
-            animator.SetBool(IS_UNDER_DIAGONAI_UP, false);
-            animator.SetBool(IS_UNDER_DIAGONAI_DOWN, false);
         }
 
-        // X축 움직임 상태 업데이트
+        // X 및 Y 이동 상태 업데이트
         animator.SetBool(IS_MOVING_X, inputVector.x != 0);
-
-        // Y축 움직임 상태 업데이트
         animator.SetBool(IS_MOVING_Y, inputVector.y != 0);
     }
 
@@ -86,6 +67,5 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 nextVector = inputVector * MoveSpeed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVector);
-        UpdateAnimationStates();
     }
 }
